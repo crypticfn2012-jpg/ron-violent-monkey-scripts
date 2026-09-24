25eac4b2ddf5564ce3fd1a8d32454b69b53b6175
Ron <crypticfn2012@gmail.com>
2026-09-24 16:59:40 +0100

    Add service-worker rule match diagnostics

--- ron-game-ad-blocker-extension/service-worker.js
+++ ron-game-ad-blocker-extension/service-worker.js
@@ -8,6 +8,19 @@ chrome.runtime.onStartup.addListener(() => {
   console.info('[Ron | Game Ad Blocker] started');
 });
 
+// Available for unpacked/developer-mode builds because the manifest includes
+// declarativeNetRequestFeedback. This gives us a real confirmation in the
+// extension service-worker console whenever one of our network rules fires.
+if (chrome.declarativeNetRequest?.onRuleMatchedDebug) {
+  chrome.declarativeNetRequest.onRuleMatchedDebug.addListener(details => {
+    console.info(
+      '[Ron | Game Ad Blocker] blocked',
+      details.rule?.ruleId,
+      details.request?.url
+    );
+  });
+}
+
 chrome.action.onClicked.addListener(() => {
   console.info('[Ron | Game Ad Blocker] active');
 });
